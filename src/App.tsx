33faf94f4f6d924c567c00edcb2e2eb29026810b
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-29 11:46:42 +0100

    feat: config vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+  };
 
   return (
     <>
       <div>
+        <h2>Envs</h2>
+        <p>ENV_TARGET_ENV: {import.meta.env.ENV_TARGET_ENV}</p>
+        <p>VITE_APP_VERSION: {import.meta.env.VITE_APP_VERSION}</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,18 +25,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={handleClick}>count is {count}</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
