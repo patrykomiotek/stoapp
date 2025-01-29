62b2bd33c205e2d5206b527f97c12d00662c5784
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-29 14:07:02 +0100

    feat: with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest-setup.ts'],
+  },
+});
