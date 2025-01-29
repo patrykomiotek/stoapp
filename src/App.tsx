bd1657840c32a9c18194e5ea001ed1fe9aae4821
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-29 12:16:03 +0100

    feat: text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+// import Kaczka from './ui/Text/Text';
+import { Text } from './ui/Text/Text';
 
-  const handleClick = () => {
-    setCount((count) => count + 1);
-  };
+function App() {
+  // const [count, setCount] = useState(0);
+
+  // const handleClick = () => {
+  //   setCount((count) => count + 1);
+  // };
 
   return (
     <>
       <div>
-        <h2>Envs</h2>
+        {/* <Text text="Hello world" /> */}
+        <Text>Hello world</Text>
+
+        {/* <h2>Envs</h2>
         <p>ENV_TARGET_ENV: {import.meta.env.ENV_TARGET_ENV}</p>
-        <p>VITE_APP_VERSION: {import.meta.env.VITE_APP_VERSION}</p>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>VITE_APP_VERSION: {import.meta.env.VITE_APP_VERSION}</p> */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>count is {count}</button>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
---
+++ src/ui/Text/Text.tsx
@@ -0,0 +1,22 @@
+// function MyText(props) {
+//   return <div>{props.message}</div>;
+// }
+
+// interface Props {
+//   text: string; // React.ReactNode
+// }
+
+// const Text : React.FC<TextProps> = ({ text }) => {
+// export const Text = ({ text }: Props) => {
+//   return <div>{text}</div>;
+// };
+
+// export default MyText;
+
+interface Props {
+  children: string; // React.ReactNode
+}
+
+export const Text = ({ children }: Props) => {
+  return <div>{children}</div>;
+};
