55c9e4615cdf31b478248959c4e5db685cc5ce1d
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-30 11:18:26 +0100

    feat: with tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
   test: {
     include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     environment: 'jsdom',
