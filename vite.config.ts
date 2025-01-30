d68cb0309a1068dbe9f0bae09f9605ea7fc3d4f9
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-30 13:40:36 +0100

    feat: allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    allowedHosts: ['37xcsh-5173.csb.app'],
+  },
   test: {
     include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     environment: 'jsdom',
