2daba3ed49e762c75b9be252c5784cc6ea5b1a97
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-30 15:24:55 +0100

    feat: with random numbers generation

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@ui';
-import { ChangeEventHandler, useState } from 'react';
+import { ChangeEventHandler, useRef, useState } from 'react';
 
-const data = [
+// [3, 2]
+
+const initialData = [
   {
     id: 3,
-    name: 'Jan Kowalski',
+    name: 'Jan K.',
     email: 'jan@wp.pl',
   },
   {
     id: 7,
-    name: 'Julia Maliniak',
+    name: 'Julia M.',
     email: 'julia@gmail.com',
   },
   {
@@ -20,29 +22,71 @@ const data = [
 ];
 
 export const Table = () => {
+  const userRefs = useRef<string[]>([]); // current=[]
   const usersIds: string[] = [];
   const [ids, setIds] = useState<string[]>([]);
+  const [painted, setPainter] = useState(true);
+  const [data, setData] = useState(initialData);
   // const ids = state[0];
   // const setIds = state[1];
   // state[1](124)
 
   const handleClick = () => {
-    alert(JSON.stringify(usersIds));
     // alert(JSON.stringify(ids));
+    // alert(JSON.stringify(usersIds));
+    alert(JSON.stringify(userRefs.current));
   };
 
   const handleCheck: ChangeEventHandler<HTMLInputElement> = (event) => {
     const isChecked = event.target.checked;
     if (isChecked) {
       // setIds([...ids, event.target.id]);
-      usersIds.push(event.target.id);
+      // ids.push(event.target.id); // Działa, ale NIE :)
+      // usersIds.push(event.target.id);
+      userRefs.current.push(event.target.id);
     }
   };
+
+  function getRandomArbitrary(min: number, max: number) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+  const handleRerender = () => {
+    // const newData = [...data];
+    // const randomId = getRandomArbitrary(0, 2);
+    // data[randomId].name = data[randomId].name;
+
+    // const clonedData = structuredClone([...data]);
+    const clonedData = structuredClone(data);
+    const newData = clonedData.map((elem) => {
+      const randomId1 = getRandomArbitrary(0, 2);
+      const randomId2 = getRandomArbitrary(0, 2);
+      console.log('randomId: ', randomId1);
+      elem.name = data[randomId1].name;
+      return {
+        ...elem,
+        // name: data[randomId1].name,
+        name: `${randomId1}`,
+        email: `${randomId2}`,
+      };
+      // return elem;
+    });
+    console.log(newData);
+    setData(newData);
+
+    // const clonedData = structuredClone(data);
+    // clonedData.s
+
+    console.log('Rerender');
+    setPainter((value) => !value);
+  };
+
+  // createRef()
 
   return (
     <div>
       <h1>Users</h1>
       <Button label="Show ids" onClick={handleClick} />
+      <Button label="Rerender" onClick={handleRerender} />
       <table>
         <thead>
           <tr>
@@ -58,6 +102,7 @@ export const Table = () => {
               <tr key={elem.id}>
                 <td style={{ padding: 4 }}>
                   <input
+                    // ref={refs[0][2]}
                     type="checkbox"
                     id={`${elem.id}`}
                     onChange={handleCheck}
