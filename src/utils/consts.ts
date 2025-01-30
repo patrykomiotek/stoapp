e4af551d1d6ca21a5daecb09e304c44bf80dbbe9
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-30 12:40:25 +0100

    feat: with branded types

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -21,3 +21,37 @@ const const3 = {
   apiKey: '1234',
   randomField: '1234',
 } satisfies Config;
+
+type TypeA = number;
+type TypeB = number;
+
+const numberA: TypeA = 5;
+const numberB: TypeB = 5;
+
+console.log(numberA == numberB); // true
+console.log(numberA === numberB); // true
+
+// if (typeof numberA === 'number') {
+
+// }
+
+type Brand<T, B> = T & { readonly __brand: B };
+
+type PLN = Brand<number, 'PLN'>;
+type USD = Brand<number, 'USD'>;
+
+const pln = 100 as PLN;
+const usd = 50 as USD;
+
+// console.log(pln + usd);
+
+class Exchange {
+  add(amount1: PLN, amount2: PLN) {
+    return amount1 + amount2;
+  }
+}
+const exchange = new Exchange();
+
+// number & { __brand: PLN }
+
+exchange.add(pln, numberB);
