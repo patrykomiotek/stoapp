2651dedaab72cd07aa11df28843a109d3b2de191
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-31 12:27:21 +0100

    feat: with Auth and KeepValue components

---
+++ src/components/Auth/AuthContext.tsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState } from 'react';
+
+type AuthContext = {
+  isAuthenticated: boolean;
+  toggle: () => void;
+  logIn: () => void;
+  logOut: () => void;
+};
+
+// default values
+const AuthContext = createContext<AuthContext | null>(null);
+
+class ReactContextError extends Error {}
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (context) {
+    return context;
+  }
+  throw new ReactContextError(
+    'Oh no! Component should be placed inside AuthProvider',
+  );
+};
+
+/**
+ * @deprecated Do not use directly!
+ * @returns
+ */
+export const useInternalAuth = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const toggle = () => setIsLoggedIn((value) => !value);
+  const logIn = () => setIsLoggedIn(true);
+  const logOut = () => setIsLoggedIn(false);
+
+  return { isLoggedIn, toggle, logIn, logOut };
+};
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { isLoggedIn, toggle, logIn, logOut } = useInternalAuth();
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated: isLoggedIn, toggle, logIn, logOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// AuthContext.Provider
+// AuthContext.displayName
