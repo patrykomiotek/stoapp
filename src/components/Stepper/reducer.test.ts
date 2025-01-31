26dc20e04fef8b513238d6bb9a2a0859a720bf51
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-01-31 11:00:34 +0100

    feat: with reducer tests

--- src/components/Stepper/reducer.test.ts
+++ src/components/Stepper/reducer.test.ts
@@ -5,5 +5,8 @@ describe('Stepper reducer', () => {
     const state = { value: 55 };
     const incResult = reducer(state, { type: ActionType.INCREMENT });
     expect(incResult.value).toEqual(56);
+
+    const setResult = reducer(state, { type: ActionType.SET, payload: 123 });
+    expect(setResult.value).toEqual(123);
   });
 });
